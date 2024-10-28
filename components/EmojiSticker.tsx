debad68a4fa8f2def7d849cde77de6b8476702e9
rcaradec <rcaradec.dev@gmail.com>
2024-10-29 00:34:55 +0100

    Add Tap Gesture scale * 2

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,21 +1,46 @@
+import { View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 import { type ImageSource } from "expo-image";
-import { Image, View } from "react-native";
 
-type EmojiStickerProps = {
+type Props = {
   imageSize: number;
   stickerSource: ImageSource;
 };
 
-export default function EmojiSticker({
-  imageSize,
-  stickerSource,
-}: EmojiStickerProps) {
+export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  const scaleImage = useSharedValue(imageSize);
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      } else {
+        scaleImage.value = Math.round(scaleImage.value / 2);
+      }
+    });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
   return (
     <View style={{ top: -350 }}>
-      <Image
-        source={stickerSource}
-        style={{ width: imageSize, height: imageSize }}
-      />
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </GestureDetector>
     </View>
   );
 }
